ff5ff5a764615018b49a6562206f2b7a5eb8a133
Phil B <phil@berryman.org.uk>
2018-09-19 18:45:44 +0100

    Dan and Phil - complete

--- src/index.js
+++ src/index.js
@@ -2,4 +2,90 @@ function add(a, b){
   return a + b;
 }
 
+function longestString(array){
+  let result = "";
+  array.forEach(word => {
+    if (word.length > result.length){
+      result = word;
+    }
+  })
+  return result
+}
+
+function toLeet(string){
+  let newString = "";
+  newString = string.replace(/i/g, 1);
+  newString = newString.replace(/l/g, 1);
+  newString = newString.replace(/z/g, 2);
+  newString = newString.replace(/e/g, 3);
+  newString = newString.replace(/a/g, 4);
+  newString = newString.replace(/s/g, 5);
+  newString = newString.replace(/g/g, 6);
+  newString = newString.replace(/t/g, 7);
+  newString = newString.replace(/y/g, 7);
+  newString = newString.replace(/b/g, 8);
+  newString = newString.replace(/q/g, 9);
+  newString = newString.replace(/o/g, 0);
+
+  
+
+  return newString;
+
+
+
+}
+
+function uniqueStrings(array){
+  let resultObj = {}
+  array.forEach(string => {
+    resultObj[string] = ""
+  })
+  return Object.keys(resultObj)
+}
+
+function Developer(name,languages){
+  this.name = name;
+  this.languages = languages;
+}
+
+Developer.prototype.learnLanguage = function(language){
+  if (! this.languages.includes(language)){
+    this.languages.push(language);
+  }
+}
+
+function stringsConcat(array) {
+  return array.reduce((a,b) => {
+    if (typeof b === 'string') {
+      return a + b;
+    }
+    return a;
+  })
+}
+
+function negativeOnly(array){
+  return array.filter(element => {
+    if (element < 0){
+      return element
+    }
+  })
+}
+
+function camelise(string){
+  array = string.split(" ");
+  capsArray = array.map(word => {
+    return word.charAt(0).toUpperCase() + word.substr(1);
+  } )
+   capsArray[0] = capsArray[0].charAt(0).toLowerCase() + capsArray[0].substr(1);
+   return capsArray.join("");
+
+}
+
 exports.add = add;
+exports.longestString = longestString
+exports.toLeet = toLeet;
+exports.uniqueStrings = uniqueStrings
+exports.Developer = Developer
+exports.stringsConcat = stringsConcat
+exports.negativeOnly = negativeOnly
+exports.camelise = camelise
--- test/index.test.js
+++ test/index.test.js
@@ -5,3 +5,66 @@ test('Addition', function(){
   const result = functions.add(2, 3);
   expect(result).toBe(expected);
 });
+
+test('Longest String', function(){
+  const expected = 'laptop';
+  const result = functions.longestString(["laptop", "dog"]);
+  expect(result).toBe(expected);
+});
+
+
+test('toLeet', function(){
+  const expected = "1337"
+  const result = functions.toLeet("leet")
+  expect(result).toBe(expected)
+})
+
+test('Unique Strings', function(){
+  const expected = ['hello','my','dog', 'cat'];
+  const result = functions.uniqueStrings(['hello','my','dog','cat','cat'])
+  expect(result).toEqual(expected)
+})
+
+test('developer', function(){
+  const expected = {name: 'Phil',
+                    languages: ['JavaScript', 'Python']}
+  const result = new functions.Developer('Phil', ['JavaScript', 'Python'])
+  expect(result).toEqual(expected)
+})
+
+test('learn - add new language', function(){
+  const phil = new functions.Developer ('Phil', ['JavaScript', 'Python'])
+  const expected = {name: 'Phil',
+                    languages: ['JavaScript', 'Python','HTML']}
+  phil.learnLanguage("HTML");
+  const result = phil
+  expect(result).toEqual(expected)
+})
+
+test('learn - dont add duplicate language', function(){
+  const phil = new functions.Developer ('Phil', ['JavaScript', 'Python', 'HTML'])
+  const expected = {name: 'Phil',
+                    languages: ['JavaScript', 'Python','HTML']}
+  phil.learnLanguage("HTML");
+  const result = phil
+  expect(result).toEqual(expected)
+})
+
+test('concat array of strings into single string', function(){
+  const expected = 'dogcatmouse'
+  const result = functions.stringsConcat(['dog', 1, 'cat', 5, 'mouse', 9])
+  expect(result).toBe(expected)
+})
+
+test('return an array of negative numbers', function() {
+  const expected = [-1,-2,-3,-4,-5];
+  const result = functions.negativeOnly([1,-1,2,-2,3,-3,4,-4,5,-5])
+  expect(result).toEqual(expected)
+})
+
+
+test('return input string in camelCase', function(){
+  const expected = 'theRainInSpainFallsMostlyOnThePlain'
+  const result = functions.camelise('the rain in spain falls mostly on the plain')
+  expect(result).toBe(expected)
+})
